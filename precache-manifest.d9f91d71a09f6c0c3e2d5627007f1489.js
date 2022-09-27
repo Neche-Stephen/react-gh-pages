d0af42dec64d2b1b409aab75d9f182b0c9b9810f
chinecherem <chinecheremubawike@gmail.com>
2022-09-27 14:47:15 -0500

    Updates

---
+++ precache-manifest.d9f91d71a09f6c0c3e2d5627007f1489.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fba4a7e862dd584279b",
+    "url": "/react-gh-pages/static/css/main.cc3fde7b.chunk.css"
+  },
+  {
+    "revision": "7fba4a7e862dd584279b",
+    "url": "/react-gh-pages/static/js/main.7fba4a7e.chunk.js"
+  },
+  {
+    "revision": "4c6cd71089cda8dc3bf0",
+    "url": "/react-gh-pages/static/css/1.de278da2.chunk.css"
+  },
+  {
+    "revision": "4c6cd71089cda8dc3bf0",
+    "url": "/react-gh-pages/static/js/1.4c6cd710.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/react-gh-pages/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "/react-gh-pages/static/media/me.0a8cf10e.png"
+  },
+  {
+    "revision": "7d1fe24b3072b92a2bd11f8a5d569f9b",
+    "url": "/react-gh-pages/static/media/me-about.7d1fe24b.jpg"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/react-gh-pages/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/react-gh-pages/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/react-gh-pages/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/react-gh-pages/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/react-gh-pages/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/react-gh-pages/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "b077b721a4f6797c3d2b5d99537bb3f6",
+    "url": "/react-gh-pages/index.html"
+  }
+];
